d557c1be0d870ce8c075b121e3991d8092e9c27b
goldyverma3132 <107996392+goldyverma3132@users.noreply.github.com>
2022-06-23 16:16:52 +0530

    router

---
+++ router.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../controller/controller');
+//import controller
+// const User = require('../controller/user');
+
+router.post('/register',User.register);
+router.post('/login',User.login);
+router.post('/uploadFile',User.uploadFile);
+router.post('/deleteFile',User.deleteFile);
+
+module.exports=router;
+
+
